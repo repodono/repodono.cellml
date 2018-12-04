353d8ca9e9de6d9bffdf398bd3d34dcc91f3490d
Tommy Yu <tommy.yu@auckland.ac.nz>
2018-12-04 18:26:37 +1300

    Bump cgrspy version
    - Ensure that the bug fixes provided by this version are used.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
-        'cgrspy',
+        'cgrspy>=1.3.0',
         'requests',
     ],
     extras_require={
